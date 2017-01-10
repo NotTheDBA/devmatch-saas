1a9d95f791a77e5911b8a2c32c133cfb260409fd
Randy Reitz <notthedba@outlook.com>
2017-01-10 19:42:07 +0000

    Added contact form mailer view.

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionMailer::Base
+    default to: 'notthedba@outlook.com'
+    def contact_email(name, mail, body)
+        @name = name
+        @email = email
+        @body = body
+        mail(from: email, subject:'Saasapp Contact Form Message')
+    end
+end
