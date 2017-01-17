e139e3cccd57dbfe27750acaf57c3e63927b5331
Randy Reitz <notthedba@outlook.com>
2017-01-17 15:45:40 +0000

    Added devise for user account management.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.0.0'
 
@@ -16,6 +15,9 @@ gem 'coffee-script-source', '1.12.2'
 gem 'coffee-rails', '4.2.1'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+
+# Use devise gem to manage user authentication.
+gem 'devise', '4.2.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails', '4.1.1'
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,21 @@
 class ContactsController < ApplicationController
+  # GET request to /contact-us
+  # Show new contact form
   def new
     @contact = Contact.new
   end
+  
+  # POST request to /contacts
   def create
+    # Assignment of form fields in to contact object
     @contact = Contact.new(contact_params)
+    #Save to the database
     if @contact.save
+      # Store form fields via parameters into variables
        name = params[:contact][:name]
        email = params[:contact][:email]
        body = params[:contact][:comments]
+       # send via contact mailer
        ContactMailer.contact_email(name, email, body).deliver
        flash[:success] = "Message sent."
        redirect_to contact_us_path
@@ -18,6 +26,7 @@ class ContactsController < ApplicationController
   end
 
 private
+  # Added strong variable permissions.
   def contact_params
      params.require(:contact).permit(:name, :email, :comments)
   end
